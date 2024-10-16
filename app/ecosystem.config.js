6a5784a02aee15338853e2306a5bb071ca662a2d
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2024-10-16 15:53:49 +0900

    fix

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -1,17 +1,17 @@
 
 module.exports = {
   apps: [{
-    // name: "helloworld-prod-js",
+    // name: "helloworld-prod-nextjs",
     // script: 'node_modules/next/dist/bin/next',
     script: 'node_modules/.bin/next',
     args: 'start',
     env_production: {
-        name: "helloworld-prod-js",
+        name: "helloworld-prod-nextjs",
         NODE_ENV: "production",
         PORT: 3000,
     },
     env_staging: {
-        name: "helloworld-prod-js-staging",
+        name: "helloworld-prod-nextjs-staging",
         NODE_ENV: "staging",
         PORT: 3001,
     }
